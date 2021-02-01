b0f88d0eb9e83ac358bdac30f4659de03b5075e8
yunwisdom <qq695926920@163.com>
2021-02-01 19:47:09 +0800

    新增rateLimit和CircuitBreaker

--- src/app.js
+++ src/app.js
@@ -12,8 +12,10 @@ const { P2cBalancer } = require('load-balancers');
 const service = require('restana')({});
 const onEnd = require('on-http-end');
 const CircuitBreaker = require('opossum');
+const rateLimit = require('express-rate-limit');
+const requestIp = require('request-ip');
 
-const defaultTarget = 'http://localhost:3000';
+const defaultTarget = 'http://localhost:3881';
 
 /** target 改为 rest_service_name  */
 const targets = [
@@ -41,43 +43,32 @@ const targets = [
 const balancer = new P2cBalancer(targets.length);
 
 const options = {
-    timeout: 1500, // If our function takes longer than "timeout", trigger a failure
+    timeout: 6000, // If our function takes longer than "timeout", trigger a failure
     errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
-    resetTimeout: 30 * 1000 // After 30 seconds, try again.
+    resetTimeout: 3000 // After 30 seconds, try again.
 }
 const breaker = new CircuitBreaker(([req, res, url, proxy, proxyOpts]) => {
     return new Promise((resolve, reject) => {
-
-        // 根据rest_service_name，从注册服务获取对应API服务地址列表
-
-        // 使用负载均衡算法，选取一个API服务地址，配置到proxy.Opts.base中
-
-        // 对此API服务地址，就行健康检查(/_health)，如果不正常，则重新选取API服务地址，并将此API地址，从服务列表中移除。如果正常，则继续执行
-
-        // 检查请求频率，如果过高，加入黑名单，黑名单失效后，移除黑名单
-
-        console.log('api request url: ' + url);
-        if (url && url.endsWith('hello') || false /** session or token 验证失效 */ ) {
-            proxyOpts.base = defaultTarget;
-        } else {
-            proxyOpts.base = targets[balancer.pick()];
-        }
-
-        proxy(req, res, url, proxyOpts)
-        onEnd(res, () => resolve()) // you can optionally evaluate response codes here...
+        proxy(req, res, url, proxyOpts);
+        onEnd(res, () => resolve()); // you can optionally evaluate response codes here...
     })
 }, options);
 
 breaker.fallback(([req, res], err) => {
     if (err.code === 'EOPENBREAKER') {
         res.send({
+            code: '503',
+            err: '服务器开小差了，请稍后尝试！',
+            success: false,
             message: 'Upps, looks like we are under heavy load. Please try again in 30 seconds!'
-        }, 503)
+        }, 503);
     }
 });
 
+console.log(defaultTarget.split(':')[2]);
+
 // mock service
-service.get('/**/*', (req, res) => res.send({ code: '099', err: 'token err.', success: false })).start(3000).then(() => console.log('Public service listening on 3000 port!'));
+service.get('/**/*', (req, res) => res.send({ code: '099', err: 'token err.', success: false })).start(parseInt(defaultTarget.split(':')[2])).then(() => console.log('Public Service Start!'));
 
 // gateway service
 gateway({
@@ -91,11 +82,28 @@ gateway({
         rateLimit({
             windowMs: 1 * 60 * 1000, // 1 minutes
             max: 1000, // limit each IP to 1000 requests per windowMs
-            handler: (req, res) => res.send('您的请求速度太快了，请稍后尝试!', 429)
+            handler: (req, res) => res.send({ code: '099', err: '您的请求速度太快了，请稍后尝试!', success: false }, 429)
         })
     ],
     routes: [{
-        proxyHandler: (...params) => breaker.fire(params),
+        proxyHandler: async(req, res, url, proxy, proxyOpts) => {
+            // 根据rest_service_name，从注册服务获取对应API服务地址列表
+
+            // 使用负载均衡算法，选取一个API服务地址，配置到proxy.Opts.base中
+
+            // 对此API服务地址，就行健康检查(/_health)，如果不正常，则重新选取API服务地址，并将此API地址，从服务列表中移除。如果正常，则继续执行
+
+            // 检查请求频率，如果过高，加入黑名单，黑名单失效后，移除黑名单
+
+            if (url && url.endsWith('hello') || false /** session or token 验证失效 */ ) {
+                proxyOpts.base = defaultTarget;
+            } else {
+                proxyOpts.base = targets[balancer.pick()];
+            }
+            console.log('backend service: ' + proxyOpts.base + url);
+            breaker.fire([req, res, url, proxy, proxyOpts]);
+            //return proxy(req, res, url, proxyOpts);
+        },
         //proxyHandler: async(req, res, url, proxy, proxyOpts) => { return proxy(req, res, url, proxyOpts); },
         prefix: '/gateway',
     }]
